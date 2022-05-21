06235b05eb84ae7c6308c657e4ed1cb5061513dc
Jesus Tang <jesutang3@gmail.com>
2022-05-21 08:06:52 -0500

    commit

--- Fundamentos de la Web/JS/Me Gusta/Me Gusta.js
+++ Fundamentos de la Web/JS/Me Gusta/Me Gusta.js
@@ -1,6 +1,12 @@
 
-var numberoflikes1 = 8
 var btn1 = document.querySelector(".btn1");
+var btn2 = document.querySelector(".btn2");
+var btn3 = document.querySelector(".btn3");
+var numberoflikes1 = 8
+var numberoflikes2 = 11
+var numberoflikes3 = 8
+
+
 function changeBtnNumberOfLikes1() {
     numberoflikes1 += 1
     btn1.innerText = numberoflikes1+" like(s)";
@@ -9,8 +15,6 @@ function changeBtnNumberOfLikes1() {
 }
 numberoflikes1 = changeBtnNumberOfLikes1()
 
-var numberoflikes2 = 11
-var btn2 = document.querySelector(".btn2");
 function changeBtnNumberOfLikes2() {
     numberoflikes2 += 1
     btn2.innerText = numberoflikes2+" like(s)";
@@ -18,8 +22,6 @@ function changeBtnNumberOfLikes2() {
 }
 numberoflikes2 = changeBtnNumberOfLikes2() 
 
-var numberoflikes3 = 8
-var btn3 = document.querySelector(".btn3");
 function changeBtnNumberOfLikes3() {
     numberoflikes3 += 1
     btn3.innerText = numberoflikes3+" like(s)";
