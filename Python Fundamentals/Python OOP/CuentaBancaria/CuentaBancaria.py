59d87dddcfdd1f6b3c63d3d3d3f5627eb5075202
Jesus Tang <jesutang3@gmail.com>
2022-06-09 17:56:33 -0500

    commit

--- Python Fundamentals/Python OOP/CuentaBancaria/CuentaBancaria.py
+++ Python Fundamentals/Python OOP/CuentaBancaria/CuentaBancaria.py
@@ -1,33 +1,40 @@
 class CuentaBancaria:
 
     todas_las_cuentas = []
-    # ¡No olvides agregar algunos valores predeterminados para estos parámetros!
-    def __init__(self, balance = 0, tasa_interes = 0.1): 
+
+    def __init__(self, balance = 0, tasa_interes = 0.01): 
         self.balance = balance
-        self.tasa_interes = tasa_interes
+        self.tasa_interes = tasa_interes + 1
         CuentaBancaria.todas_las_cuentas.append(self)
 
-        # tu código aquí (recuerda, los atributos de instancia van aquí)
-    # no te preocupes por la información del usuario aquí; pronto involucraremos la clase de usuario
+
     def depósito(self, amount):
         self.balance += amount
-    # tu código aquí
+        return self
+
     def retiro(self, amount):
         self.balance -= amount
-    # tu código aquí
+        return self
+
     def mostrar_info_cuenta(self):
         print(f'Balance: ${self.balance}')
+        print(f'Esta cuenta tiene como tasa de interes {self.tasa_interes}')
+        return self
+
+    def generar_interés(self):
+        self.balance = self.balance*self.tasa_interes
+        return self
+
     @classmethod
     def showAllBalances(cls):
         for cuenta in cls.todas_las_cuentas:
             print(f'{cuenta} balance is: {cuenta.balance}')
 
-    def generar_interés(self):
-        self.balance = self.balance*self.tasa_interes
-    # tu código aquí
+Jesus = CuentaBancaria(500, 0.01)
+Jose = CuentaBancaria(1000, 0.05)
 
-Jesus = CuentaBancaria(500, )
-Jose = CuentaBancaria(1000, )
-Jesus.mostrar_info_cuenta()
+Jesus.depósito(500).depósito(250).retiro(150).generar_interés().mostrar_info_cuenta()
+
+Jose.depósito(2000).depósito(2500).retiro(690).retiro(200).retiro(300).retiro(150).generar_interés().mostrar_info_cuenta()
 
 CuentaBancaria.showAllBalances()
