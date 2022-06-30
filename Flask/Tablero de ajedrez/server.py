7d23a987b70c2bc3f6eac6ad42cdef43be24b0a8
Jesus Tang <jesutang3@gmail.com>
2022-06-29 23:50:07 -0500

    commit

--- Flask/Tablero de ajedrez/server.py
+++ Flask/Tablero de ajedrez/server.py
@@ -9,11 +9,17 @@ def chekerboard():
 
 @app.route('/<int:x>')
 def checkerboard_x(x):
-    return render_template('index.html', x = x, y = 8, color1 = 'red',color2 = 'blue')
+    return render_template('index.html', x = x, y = x, color1 = 'red',color2 = 'blue')
 
 @app.route('/<int:x>/<int:y>')
 def checkerboard_x_y(x,y):
     return render_template('index.html', x = x, y = y, color1 = 'red',color2 = 'blue')
+
+@app.route('/<int:x>/<int:y>/<color1>/<color2>')
+def checkerboard_x_y_colors(x,y,color1,color2):
+    return render_template('index.html', x = x, y = y, color1 = color1,color2 = color2)
+
+
 
 
 if __name__=="__main__":
