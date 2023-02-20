b1917670d6cc17bcb8dbac3cbef246cdc6fd5915
Jesus Tang <jesutang3@gmail.com>
2023-02-19 22:17:23 -0500

    [TAREA] sensei

--- MERN/Practice assignments/ninja.js
+++ MERN/Practice assignments/ninja.js
@@ -19,3 +19,23 @@ class Ninja {
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
+
+class Sensei extends Ninja {
+    constructor(nombre) {
+        super(nombre);
+        this.nombre = nombre;
+        this.salud = 200;
+        this.velocidad = 10;
+        this.fuerza = 10;
+    }
+    speakWisdom() {
+        super.drinkSake();
+        console.log(`Lo que un programador peude hacer en un mes, dos programadores pueden hacerlo en dos meses.`)
+    }
+}
+// ejemplo de salida
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
