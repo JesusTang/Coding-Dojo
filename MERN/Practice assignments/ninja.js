7a1b299692c1b81d05625d4adc9c93c89eda639f
Jesus Tang <jesutang3@gmail.com>
2023-02-19 22:18:09 -0500

    [TAREA] sensei

--- MERN/Practice assignments/ninja.js
+++ MERN/Practice assignments/ninja.js
@@ -27,6 +27,7 @@ class Sensei extends Ninja {
         this.salud = 200;
         this.velocidad = 10;
         this.fuerza = 10;
+        this.sabiduria = 10;
     }
     speakWisdom() {
         super.drinkSake();
