168883d83c6505e09afb34c4831d3db6e43bcde1
Jesus Tang <jesutang3@gmail.com>
2023-02-25 23:05:31 -0500

    Apóyalo

---
+++ test/src/components/PersonCard.js
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+
+class PersonCard extends Component {
+    state = {  } 
+    render() { 
+        const { firstName, lastName, age, hairColor } = this.props;
+        return (
+            <div>
+                <h1>
+                {lastName}, {firstName}   
+                </h1>
+                <p>age: {age}</p>
+                <p>Hair Color: {hairColor}</p>
+            </div>
+        );
+    }
+}
+
+export default PersonCard;
