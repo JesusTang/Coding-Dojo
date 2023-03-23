c1873f8f353d163bd44c405e83070b9a8753148b
Jesus Tang <jesutang3@gmail.com>
2023-03-23 10:17:25 -0500

    Pokemones corregido

--- test/src/App.js
+++ test/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 // import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 // import PersonCard from './components/Gran Inversion/PersonCard';
 // import User from './components/Formulario con Hooks/User';
-// import Pokemones from './components/Pokemones/Pokemones'
+import Pokemones from "./components/Pokemones/Pokemones";
 // import Form from './components/Formulario con Hooks/Form';
 // import Lista from './components/Lista de Quehaceres/Lista de Quehaceres';
 // import Index from './components/React Routing/Index';
@@ -12,16 +12,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 // import Bloques from './components/React Routing/Bloques';
 // import Categories from './components/React Routing/pages/Categories';
 // import Pokemones from './components/Pokemones/Pokemones';
-import APIWalker from './components/React Routing/APIWalker';
+import APIWalker from "./components/React Routing/APIWalker";
 // import LukeAPIwalker from './components/React Routing/LukeAPIwalker';
 
 function App() {
   return (
-    <div className='App'>
-      <APIWalker/>
+    <div className="App">
+      <Pokemones />
+      {/* <APIWalker/> */}
     </div>
   );
 }
 
 export default App;
-
--- test/src/components/Pokemones/Pokemones.js
+++ test/src/components/Pokemones/Pokemones.js
@@ -1,29 +1,49 @@
-import { useState} from 'react';
-// import axios from 'axios';
+import axios from "axios";
+import { useState } from "react";
 
 function Pokemones() {
-    const [pokemones, setPokemones] = useState([])
-    const [numero, setNumero] = useState(0)
-    const go_fetch_pokemon = (ev) => {
-        ev.preventDefault()
-        fetch('https://pokeapi.co/api/v2/pokemon?limit='+numero+'/')
-            .then(response => response.json())
-            .then(response => setPokemones(response.results))
-    };
+  const [pokemones, setPokemones] = useState([]);
+  const [numero, setNumero] = useState(0);
+  // const go_fetch_pokemon = (ev) => {
+  //   //with fetch
+  //   ev.preventDefault();
+  //   fetch("https://pokeapi.co/api/v2/pokemon?limit=" + numero + "/")
+  //     .then((response) => response.json())
+  //     .then((response) => setPokemones(response.results));
+  // };
+  const go_fetch_pokemon = (ev) => {
+    // with axios
+    ev.preventDefault();
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=" + numero + "/")
+      // .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setPokemones(response.data.results);
+      });
+  };
+  console.log(numero);
 
-    console.log(pokemones)
-
-    return ( 
-        <div>
-            <form onSubmit={go_fetch_pokemon}>
-                <input type='text' value={numero} onChange={ev => setNumero(ev.target.value)}/>
-            <button type='submit'>Fetch Pokemon</button>
-            </form>
-            {pokemones.map((pokemon, index)=>{
-                return (<div key={index}>{pokemon.name}</div>)
-            })}
-        </div>
-    );
+  return (
+    <div>
+      <form onSubmit={(ev) => go_fetch_pokemon(ev)}>
+        <input
+          type="text"
+          value={numero}
+          onChange={(ev) => setNumero(ev.target.value)}
+        />
+        <button type="submit">Fetch Pokemon</button>
+      </form>
+      {pokemones.map((pokemon, index) => {
+        return (
+          <div key={index}>
+            {index}
+            {pokemon.name}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Pokemones;
