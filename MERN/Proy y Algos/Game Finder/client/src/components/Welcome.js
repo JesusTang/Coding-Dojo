03a0b31de486860796d9fae23cf1ebf59a88b77a
Jesus Tang <jesutang3@gmail.com>
2023-04-20 07:51:07 -0500

    Welcome component added

---
+++ MERN/Proy y Algos/Game Finder/client/src/components/Welcome.js
@@ -0,0 +1,11 @@
+function Welcome(props) {
+  return (
+    <div>
+      <h1 className="text-center text-white title-shadow">
+        Welcome to Sports Findr
+      </h1>
+    </div>
+  );
+}
+
+export default Welcome;
--- MERN/Proy y Algos/Game Finder/client/src/views/HomePage.js
+++ MERN/Proy y Algos/Game Finder/client/src/views/HomePage.js
@@ -1,11 +1,15 @@
-import My2ndMap from "../components/Map2ndMap";
+import My2ndMap from "../components/My2ndMap";
 import MyMap from "../components/MyMap";
 import SideBar from "../components/SideBar";
+import Welcome from "../components/Welcome";
 
 function HomePage(props) {
   return (
     <div className="p-7">
-      <div className="bg-purple row rounded-5">
+      <div className="row">
+        <Welcome />
+      </div>
+      <div className="bg-purple row rounded-5 box-shadow">
         <SideBar />
         <My2ndMap />
         {/* <MyMap /> */}
